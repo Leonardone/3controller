cc6b4cfb766f943b87cb2932f6260780d71e6503
leo <njuccon@hotmail.it>
2017-03-13 12:56:15 +0100

    Prime prove Angular

--- item.controller.js
+++ item.controller.js
@@ -1,4 +1,10 @@
 // CREA UN CONTROLLER PER L'OGGETTO
+angular.module('app').controller('ItemController',function($scope){
+
+    $scope.titolo="El Fusco";
+
+
+
 
 
 
@@ -11,3 +17,24 @@
  */
 
 //
+$scope.listaArmi= [{
+    id: '01',
+    nome: 'Ascia bipenne',
+    descrizione: 'Eddie Fuscaldo, Dicembre 2001',
+    slots: '3',
+    image: 'http://www.skuola.net/news_foto/2016/scemo-buona.jpg'
+},{
+    id: '02',
+    nome: 'Ascia bimatite',
+    descrizione: 'Edoardo Roncaldo, Dicembre 2001',
+    slots: '7',
+    image: 'http://www.repubblica.it/images/2010/12/26/101517923-c1326a40-e9e3-4c5b-865f-c1d98bbcb80f.jpg'
+}];
+
+$scope.elimina= function(id, indice){
+    console.log(id, indice);
+    $scope.listaArmi.splice(indice, 1);
+}
+
+// $scope.listanumeri= [1,2,3,4,5];
+});
